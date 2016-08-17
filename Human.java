8777e60282853ed6137f748cf5ca7874e8e73aaa
Jaime Santiesteban <jaimesantiesteban07@gmail.com>
2016-08-17 18:46:27 -0400

    java excersice

---
+++ Human.java
@@ -0,0 +1,11 @@
+public class Human extends Mammal{
+    @Override
+    public void move(){
+        System.out.println("Moving");
+    }
+
+    @Override
+    public void eat(){
+        System.out.println("Eating");
+    }
+}
